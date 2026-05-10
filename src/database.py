b7de28f8f6ea16149ec91d969089d1e78f6b6cba
Fernando <srnando4312@gmail.com>
2026-05-10 00:36:44 -0300

    feat: adiciona .env e lib para conseguir ler dados do .env

---
+++ src/database.py
@@ -0,0 +1,19 @@
+import os
+import psycopg2
+import dotenv from load_dotenv
+
+load_dotenv()
+
+def create_connection():
+    try:
+        conn = psycopg2.connect(
+            dbname=os.environ.get('DB_NAME'),
+            user=os.environ.get('DB_USER'),
+            password=os.environ.get('DB_PASS'),
+            host='localhost',
+            port='5432'
+        )
+        return conn
+
+    except Exception as e:
+        print(e)
